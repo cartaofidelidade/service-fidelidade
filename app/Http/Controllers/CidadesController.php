29943a733c5307206c15945d5c123cc1b10ad542
Thiago Rodrigues <thiago.dinnes@gmail.com>
2021-01-20 23:36:05 -0300

    Ajustes rotas

--- app/Http/Controllers/CidadesController.php
+++ app/Http/Controllers/CidadesController.php
@@ -1,25 +1,21 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\CidadesModel;
-    use Illuminate\Http\Request;
+use App\Models\CidadesModel;
+use Illuminate\Http\Request;
 
-    class CidadesController extends Controller
+class CidadesController extends Controller
+{
+    public function index()
     {
-
-        public function index()
-        {
-
-            $cidades = CidadesModel::where(['Ativo' => 1])->get();
-            return response()->json($cidades, 200);
-
-        }
-
-        public function show()
-        {
-            $cidades = CidadesModel::where(['Ativo' => 1])->get();
-            return response()->json($cidades, 200);
-        }
-
+        $cidades = CidadesModel::where(['Ativo' => 1])->get();
+        return response()->json($cidades, 200);
     }
+
+    public function show()
+    {
+        $cidades = CidadesModel::where(['Ativo' => 1])->get();
+        return response()->json($cidades, 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,54 @@
 <?php
 
-    if (isset($router)) {
+if (isset($router)) {
+    $router->get("/", "FidelidadeController@index");
 
-        $router->get("/", "FidelidadeController@index");
-        $router->get("/api/fidelidade/estados", "EstadosController@index");
-        // $router->get("/api/fidelidade/individuos", "IndividuosController@index");
+    $router->group(['prefix' => "/api"], function () use ($router) {
 
-        // $router->get("/api/fidelidade/clientes/{regis}", "ClientesController@index");
+        $router->get("/fidelidade/estados", "EstadosController@index");
+        $router->get("/fidelidade/cidades", "CidadesController@index");
 
-        /**
-         * Agrupamento de Router
-         */
-        // $router->group(['prefix' => "/api/fidelidade"], function () use ($router) {
-        // });
+        $router->group(['prefix' => "/conta", 'middleware' => 'checkToken'], function () use ($router) {
+            $router->post("/cadastro", "");
+            $router->post("/verifica-cadastro", "");
 
-        $router->group(['prefix' => "/api"], function () use ($router) {
-            
-            $router->group(['prefix' => "/conta", 'middleware' => 'checkToken'], function () use ($router) {
-                $router->post("/cadastro", "");
-                $router->post("/verifica-cadastro", "");
+            $router->post("/login", "");
 
-                $router->post("/login", "");
-
-                $router->post("/recuperar-senha", "");
-                $router->post("/recuperar-senha/{codigo}", "");
-            });
-
-            $router->group(['prefix' => "/perfil", 'middleware' => 'checkToken'], function () use ($router) {
-                $router->get('/{Id}', '');
-                $router->put('/{Id}', '');
-            });
-
-            $router->group(['prefix' => "/estabelecimentos"], function () use ($router) {             
-                $router->get("listaEstabelecimentos/{router}", "EstabelecimentosController@listaEstabelecimentos");
-                $router->post("cadastro/", "EstabelecimentosController@cadastro");
-               
-            });
-            $router->group(['prefix' => "/clientes"], function () use ($router) {              
-                $router->get("listaClientes/{router}", "ClientesController@listaClientes");
-                $router->post("cadastro/", "ClientesController@cadastro");
-                
-            });
-
-            $router->group(['prefix' => "/IndividuosContatos"], function () use ($router) {
-                $router->get("/", "IndividuosContatosController@index");
-                $router->get("buscaContatos/{router}", "IndividuosContatosController@buscaContatos");
-                $router->post("cadastro/", "IndividuosContatosController@cadastro");
-                $router->put("atualizar/{router}", "IndividuosContatosController@update");
-                $router->delete("delete/{router}", "IndividuosContatosController@destroy");
-            });
-
-            $router->group(['prefix' => "/IndividuosEnderecos"], function () use ($router) {
-                $router->get("/", "IndividuosEnderecosController@index");
-                $router->get("buscaContatos/{router}", "IndividuosEnderecosController@buscaEnderecos");
-                $router->post("cadastro/", "IndividuosEnderecosController@cadastro");
-                $router->put("atualizar/{router}", "IndividuosEnderecosController@update");
-                $router->delete("delete/{router}", "IndividuosEnderecosController@destroy");
-            });
+            $router->post("/recuperar-senha", "");
+            $router->post("/recuperar-senha/{codigo}", "");
         });
-    }
+
+        $router->group(['prefix' => "/perfil", 'middleware' => 'checkToken'], function () use ($router) {
+            $router->get('/{Id}', '');
+            $router->put('/{Id}', '');
+        });
+
+        $router->group(['prefix' => "/estabelecimentos"], function () use ($router) {
+            $router->get("listaEstabelecimentos/{router}", "EstabelecimentosController@listaEstabelecimentos");
+            $router->post("cadastro/", "EstabelecimentosController@cadastro");
+
+        });
+
+        $router->group(['prefix' => "/clientes"], function () use ($router) {
+            $router->get("listaClientes/{router}", "ClientesController@listaClientes");
+            $router->post("cadastro/", "ClientesController@cadastro");
+
+        });
+
+        $router->group(['prefix' => "/IndividuosContatos"], function () use ($router) {
+            $router->get("/", "IndividuosContatosController@index");
+            $router->get("buscaContatos/{router}", "IndividuosContatosController@buscaContatos");
+            $router->post("cadastro/", "IndividuosContatosController@cadastro");
+            $router->put("atualizar/{router}", "IndividuosContatosController@update");
+            $router->delete("delete/{router}", "IndividuosContatosController@destroy");
+        });
+
+        $router->group(['prefix' => "/IndividuosEnderecos"], function () use ($router) {
+            $router->get("/", "IndividuosEnderecosController@index");
+            $router->get("buscaContatos/{router}", "IndividuosEnderecosController@buscaEnderecos");
+            $router->post("cadastro/", "IndividuosEnderecosController@cadastro");
+            $router->put("atualizar/{router}", "IndividuosEnderecosController@update");
+            $router->delete("delete/{router}", "IndividuosEnderecosController@destroy");
+        });
+    });
+}
