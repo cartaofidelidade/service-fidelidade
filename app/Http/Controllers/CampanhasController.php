3f91dfd18d427bc919e8e63d52e9f99c0b0cf7d8
MacBook Pro <thiago.dinnes@gmail.com>
2021-04-03 14:47:15 -0300

    Ajustes

--- app/Http/Controllers/CampanhasController.php
+++ app/Http/Controllers/CampanhasController.php
@@ -2,53 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Campanhas;
 use App\Models\CampanhasModel;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class CampanhasController extends Controller
 {
 
-    public function index(Request $request)
+    public function index(Response $response)
     {
-        $params = $request->query();
+        $params = [];
 
-        if (!isset($params['Estabelecimento']))
-            return response()->json(['mensagem' => 'Parametros obrigatórios ibválidos.'], 400);
+        $params['ativo'] = 1;
 
-        $campanhas = CampanhasModel::where([
-            'Ativo' => 1,
-            'EstabelecimentosId' => $params['Estabelecimento']
-        ])
-            ->get();
-
-        return response()->json($campanhas, 200);
+        $campanhas = Campanhas::where($params)->get();
+        return response()->json($campanhas);
     }
 
-    public function show(Request $request, $id)
+    public function show($id)
     {
-        $params = $request->query();
-
-        if (!isset($params['Estabelecimento']))
-            return response()->json(['mensagem' => 'Parametros obrigatórios ibválidos.'], 400);
-
-        $campanhas = CampanhasModel::where([
-            'Id' => $id,
-            'EstabelecimentosId' => $params['Estabelecimento']
-        ])
-            ->first();
-
-        return response()->json($campanhas, 200);
+        $campanhas = Campanhas::find($id);
+        return response()->json($campanhas);
     }
 
     public function store(Request $request)
     {
+        $estabelecimento = Auth::user();
+        $formData = $request->all();
+
+        $validation = Validator::make(
+            $formData,
+            [
+                'nome' => 'required',
+                'tipo' => 'required',
+                'data_inicio' => 'required',
+                'data_final' => 'required'
+            ],
+            [
+                'required' => 'O campo :atribute é obrigatório'
+            ]
+        );
+
+        if ($validation->fails())
+            return response()->json(['status' => 'erro', 'mensagem' => $validation->errors()->first()], 400);
+
+         $campanhas = new Campanhas();
+
+         $campanhas->estabelecimentos_id = $estabelecimento->origem_id;
+         $campanhas->codigo = substr(uniqid(rand()), 0, 6);
+         $campanhas->nome = $formData['nome'];
+         $campanhas->tipo = $formData['tipo'];
+
+         $campanhas->pontos = $formData['pontos']??0;
+         $campanhas->quantidade_carimbos = $formData['quantidade_carimbos'];
+         $campanhas->limite_carimbos_dia = $formData['limite_carimbos_dia'];
+         $campanhas->data_inicio = $formData['data_inicio'];
+         $campanhas->data_final = $formData['data_final'];
+         $campanhas->descricao = $formData['descricao'];
+
+         if ($campanhas->save())
+            return response()->json($campanhas);
+        return response()->json(['status' => 'erro', 'mesnagem' => 'Não foi possível cadastrar a campanha.'], 400);
+
+
     }
 
-    public function update(Request $request, $id)
+    public function update()
     {
     }
 
-    public function delete($id)
+    public function delete()
     {
     }
 }
