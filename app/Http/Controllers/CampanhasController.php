5714573cf320242f68facdaf42aed75ad8ff0f27
Regis Nunes <regisnunes2008@hotmail.com>
2021-07-13 07:44:38 -0300

    funcao de upload

--- app/Http/Controllers/CampanhasController.php
+++ app/Http/Controllers/CampanhasController.php
@@ -42,45 +42,49 @@ class CampanhasController extends Controller
     public function store(Request $request)
     {
 
-        $estabelecimento = Auth::user();
-        $formData = $request->all();
+        try {
+            $estabelecimento = Auth::user();
+            $formData = $request->all();
 
-        $validation = Validator::make(
-            $formData,
-            [
-                'nome' => 'required',
-                'tipo' => 'required',
-                'data_inicio' => 'required',
-                'data_final' => 'required'
-            ],
-            [
-                'required' => 'O campo :atribute é obrigatório'
-            ]
-        );
+            $validation = Validator::make(
+                $formData,
+                [
+                    'nome' => 'required',
+                    'tipo' => 'required',
+                    'data_inicio' => 'required',
+                    'data_final' => 'required'
+                ],
+                [
+                    'required' => 'O campo :atribute é obrigatório'
+                ]
+            );
 
-        if ($validation->fails())
-            return response()->json(['status' => 'erro', 'mensagem' => $validation->errors()->first()], 400);
+            if ($validation->fails())
+                return response()->json(['status' => 'erro', 'mensagem' => $validation->errors()->first()], 400);
 
-        $campanhas = new Campanhas();
+            $campanhas = new Campanhas();
 
-        $campanhas->estabelecimentos_id = $estabelecimento->origem_id;
-        $campanhas->codigo = substr(uniqid(rand()), 0, 6);
-        $campanhas->nome = $formData['nome'];
-        $campanhas->tipo = $formData['tipo'];
+            $campanhas->estabelecimentos_id = $estabelecimento->origem_id;
+            $campanhas->codigo = substr(uniqid(rand()), 0, 6);
+            $campanhas->nome = $formData['nome'];
+            $campanhas->tipo = $formData['tipo'];
 
-        $campanhas->pontos = $formData['pontos'] ?? 0;
-        $campanhas->quantidade_carimbos = $formData['quantidade_carimbos'];
-        $campanhas->limite_carimbos_dia = $formData['limite_carimbos_dia'];
-        $campanhas->data_inicio = $formData['data_inicio'];
-        $campanhas->data_final = $formData['data_final'];
-        $campanhas->descricao = $formData['descricao'];
+            $campanhas->pontos = $formData['pontos'] ?? 0;
+            $campanhas->quantidade_carimbos = $formData['quantidade_carimbos'];
+            $campanhas->limite_carimbos_dia = $formData['limite_carimbos_dia'];
+            $campanhas->data_inicio = $formData['data_inicio'];
+            $campanhas->data_final = $formData['data_final'];
+            $campanhas->descricao = $formData['descricao'];
 
-        $campanhas->imagem_carimbo_preenchido = $this->uploadImagem($formData['imagem_carimbo_preenchido']) ?? null;
-        $campanhas->imagem_carimbo_vazio = $this->uploadImagem($formData['imagem_carimbo_vazio']) ?? null;
+            $campanhas->imagem_carimbo_preenchido = $this->uploadImagem($formData['imagem_carimbo_preenchido']) ?? null;
+            $campanhas->imagem_carimbo_vazio = $this->uploadImagem($formData['imagem_carimbo_vazio']) ?? null;
 
-        if ($campanhas->save())
-            return response()->json($campanhas);
-        return response()->json(['status' => 'erro', 'mesnagem' => 'Não foi possível cadastrar a campanha.'], 400);
+            if ($campanhas->save())
+                return response()->json($campanhas);
+            return response()->json(['status' => 'erro', 'mesnagem' => 'Não foi possível cadastrar a campanha.'], 400);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 'erro', 'mesnagem' => $th], 400);
+        }
     }
 
     public function update(Request $request, $id)
@@ -122,6 +126,7 @@ class CampanhasController extends Controller
 
             if ($campanhas->save())
                 return response()->json($campanhas);
+
             return response()->json(['status' => 'erro', 'mensagem' => 'Não foi possível cadastrar a campanha.'], 400);
         } catch (\Throwable $th) {
             return response()->json(['status' => 'erro', 'mensagem' => $th], 400);
@@ -137,15 +142,19 @@ class CampanhasController extends Controller
 
             if ($campanhas->save())
                 return response()->json($campanhas);
-                
         } catch (\Throwable $th) {
-            return response()->json(['status', 'erro', 'mensagem' => $th], 400);
+            return response()->json(['status', 'erro', 'mensagem' => '', $th], 400);
         }
     }
 
-    private function uploadImagem($file = null, $path = null): string
+    private function uploadImagem($file = null, $path = null)
     {
-        if (is_null($file) || is_null($path))
-            return false;
+        // if(strpos($file, ';base64')){
+        //     return response()->json(['status' => 'erro', 'mensagem' => 'Regis nunes'], 400);
+            
+
+
+        // if (is_null($file) || is_null($path))
+        //     return false;
     }
 }
---
+++ app/Http/Controllers/CartelasController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cartelas;
+use Illuminate\Http\Request;
+
+class CidadesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $params = [];
+
+        if (isset($request->nome) && !empty($request->nome))
+            $params['nome'] = $request->nome;
+
+        if (isset($request->estadoId) && !empty($request->estadoId))
+            $params['estados_id'] = $request->estadoId;
+
+        if (isset($request->id) && !empty($request->id))
+            $params['id'] = $request->id;
+
+
+           
+        $cidades = Cartelas::where($params)->orderBy('nome')->get();
+        return response()->json($cidades);
+    }
+
+    public function store(Request $request)
+    {
+        $cartela = new Cartelas();
+
+        $cartela->campanhas_id = '';
+        $cartela->clientes_id = '';
+
+    }
+
+    public function validaCarimbos()
+    {
+        # code...
+    }
+}
